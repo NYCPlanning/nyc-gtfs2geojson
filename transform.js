ffe887ef384845766d592baedfd96a37208a2683
Chris Whong <chris@whong.org>
2016-07-11 16:07:17 -0400

    combined shapes and stops into one script

--- transform.js
+++ transform.js
@@ -12,14 +12,22 @@ sources.forEach(function(source) {
   //open shapes.txt, specify utf-8 encoding so we get a string back, not a buffer
   var text = fs.readFileSync( './temp/' + source.name + '/stops.txt','utf8' );
 
-  //pass text to gtfs2geojson, stringify resulting object, write to file
-  var geoJson = gtfs2geojson.stops( text );
 
+  if (!fs.existsSync('geojson/' + source.name)){
+      fs.mkdirSync('geojson/' + source.name);
+  }
 
-  var output = fs.createWriteStream('geojson/' + source.name + '/stops.geojson')
+  //transform shapes.txt
+  var geoJson = gtfs2geojson.lines( text );
+  var output = fs.createWriteStream('geojson/' + source.name + '/lines.geojson')
   output.write(JSON.stringify(geoJson));
 
 
+  //transform stops.txt
+  var geoJson = gtfs2geojson.stops( text );
+  var output = fs.createWriteStream('geojson/' + source.name + '/stops.geojson')
+  output.write(JSON.stringify(geoJson));
+  
 });
 
 
